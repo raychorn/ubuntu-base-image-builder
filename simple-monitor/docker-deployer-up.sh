9270939759929a824e40f0d9765ac37be137ea00
Ray C Horn <raychorn@gmail.com>
2021-09-19 09:49:25 -0600

    updates.

---
+++ simple-monitor/docker-deployer-up.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CWD=$(dirname "$0")
+PWD=$(pwd)
+
+if [ "$CWD." == ".." ]; then
+    CWD=$PWD
+    echo "CWD:$CWD"
+fi
+
+echo "CWD:$CWD"
+
+CNAME=nagios4
+CID=$(docker ps | grep $CNAME | awk '{print $1}')
+
+export CWD=$CWD
+
+if [ -z "$CID" ]; then
+    docker-compose -f ./docker-deployer-compose.yml up -d
+else
+    docker-compose -f ./docker-deployer-compose.yml down
+fi
+  
